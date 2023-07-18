b3cf27972655f8dfe9584bdc9e4808066755d945
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2023-07-18 14:13:39 +1200

    Swapped HDRP for URP

---
+++ Lost n Found/Assets/Blue/Scripts/Menu/Buttons/JoinCodeButton.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class JoinCodeButton : BasicButton
+{
+    NetworkHelper networkConnectionData;
+
+    [SerializeField]
+    private VisibilityButton visibilityButton;
+
+    [Inject]
+    public void Construct(NetworkHelper networkConnectionData)
+    {
+        this.networkConnectionData = networkConnectionData;
+    }
+
+    protected override async void OnClick()
+    {
+        bool result = await networkConnectionData.Host(visibilityButton.IsPrivate);
+
+        if (result)
+        {
+            base.OnClick();
+        }
+        else
+        {
+            ButtonsFrozen = false;
+        }
+    }
+}
