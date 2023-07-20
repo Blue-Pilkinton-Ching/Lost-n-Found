116eb32b3272cdb6f03e442c6a48557ec0b6b171
BloobyJoobyJoob <bluepilkintonching@gmail.com>
2023-07-21 01:29:58 +1200

    Started work with vivox

--- Lost n Found/Assets/Blue/Scripts/Managers/VivoxManager.cs
+++ Lost n Found/Assets/Blue/Scripts/Managers/VivoxManager.cs
@@ -2,18 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Services.Vivox;
 using UnityEngine;
+using VivoxUnity;
+using System;
 
 public class VivoxManager : MonoBehaviour
 {
+    ILoginSession loginSession;
+
+
+
+    Account account;
     private void Awake() {
         DontDestroyOnLoad(this);
     }
 
     public void Initialize() 
     {
+        Client client = new Client();
+        client.Initialize();
+
         VivoxService.Instance.Initialize();
 
-        Account account = new Account();
-        //LoginSession = Vi
+        account = new Account(Guid.NewGuid().ToString());
+        loginSession = client.GetLoginSession(account);
+        
+        //loginSession.BeginLogin(_serverUri, loginSession.GetLoginToken(_tokenKey, _tokenExpiration), ar => SignInCompleteAsyncCallback(ar));
+    }
+
+    private void SignInCompleteAsyncCallback(IAsyncResult result) 
+    {
+        try
+        {
+            loginSession.EndLogin(result);
+        }
+        catch
+        {
+            // Handle error
+            throw;
+        }
+        // At this point, login is successful and other operations can be performed.
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Lost n Found
 
 Unity Version 2022.3.4f
-dfgsdfg
+
+During Production build:
+- Uncheck Test Mode in Vivox project settings
+- Change Authentication to not create a random profile
+- Change Vivox to not be creating development tolkens using LoginSession.GetLoginToken()
+ https://docs.vivox.com/v5/general/unity/5_15_0/en-us/Default.htm#Unity/developer-guide/vivox-unity-sdk-basics/sign-in-to-game.htm?TocPath=Vivox%2520Unity%2520SDK%2520documentation%257CVivox%2520Unity%2520Developer%2520Guide%257CClient%2520SDK%2520basics%257C_____3
+
+ 
